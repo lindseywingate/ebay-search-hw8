de1fa21c4a8fe2f89b0c986b2559741a32e1e1c9
L W <lindseymwingate@gmail.com>
2020-06-16 19:17:02 -0700

    added select with values

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,16 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SearchHomeComponent } from './search-home/search-home.component';
 import { ResultsViewComponent } from './results-view/results-view.component';
+import { KeywordValidationComponent } from './keyword-validation/keyword-validation.component';
+import { PriceValidationComponent } from './price-validation/price-validation.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     SearchHomeComponent,
-    ResultsViewComponent
+    ResultsViewComponent,
+    KeywordValidationComponent,
+    PriceValidationComponent
   ],
   imports: [
     BrowserModule,
--- src/assets/js/hw8.js
+++ src/assets/js/hw8.js
@@ -1,12 +1,17 @@
 $(document).ready(function() {
-
+$('#keyword-alert').hide();
+$('#price-alert').hide();
 $('#search-button').click(function() {
     event.preventDefault();
+    var keywords = document.getElementById('keywords').value; 
+    var startRange = document.getElementById('start-range').value;
+    var endRange = document.getElementById('end-range').value;
+    if(!keywords)
+        $('#keyword-alert').show();
+    if(startRange<0 || endRange<0 || startRange>endRange)
+        $('#price-alert').show();
 
-/*    $.get('/cat', (data) => {
-        console.log('data', data);
-    });
-*/    
+
     $.ajax({
         url: 'http://localhost:3000/cat',
         method: 'GET',
@@ -15,5 +20,10 @@ $('#search-button').click(function() {
         }
     })
 })
+
+$('#clear-button').click(function() {
+    $('#keyword-alert').hide();
+    $('#price-alert').hide();   
+}) 
 
 });
